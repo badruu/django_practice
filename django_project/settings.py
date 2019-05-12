0da5d2b484f5943c07ccaf956d929c4250bfbdaa
projects <projects@Salaaxs-MacBook-Air.local>
2019-05-12 15:16:05 +0300

    set debug valie

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = (config('DEBUG_VALUE') == "True")
 
 MODE='dev'
 
